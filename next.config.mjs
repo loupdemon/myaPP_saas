280aa2b36834edceccf253207231577755e51b86
Aghilas AZZOUG <azzougaghiles@hotmail.com>
2025-07-09 00:38:13 +0200

    correct config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Activer l'exportation statique
-    exportTrailingSlash: true, // Ajoute un / à la fin des URLs
-    distDir: "build", // Choisit un dossier de sortie personnalisé (ici 'build')
-    // Si vous avez d'autres configurations, vous pouvez les ajouter ici.
-};
+const nextConfig = {};
 
 export default nextConfig;
